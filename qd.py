e9194124bdaa9fc5fa620c2ffec72c97c4530861
godad <314132406@qq.com>
2016-10-27 14:20:13 +0800

    Signed-off-by: silence <314132406@qq.com>

--- qd.py
+++ qd.py
@@ -8,11 +8,11 @@ import time
 hosturl = 'http://123.206.92.65' 
 posturl = 'http://123.206.92.65/web/useraction.php?a=dologin' 
   
-cj = cookielib.LWPCookieJar()  
-cookie_support = urllib2.HTTPCookieProcessor(cj)  
+cook = cookielib.LWPCookieJar()  
+cookie_support = urllib2.HTTPCookieProcessor(cook)  
 opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)  
 urllib2.install_opener(opener)  
-h = urllib2.urlopen(hosturl)  
+host = urllib2.urlopen(hosturl)  
   
 headers = {'User-Agent' : 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',  
            'Referer' : 'http://123.206.92.65/web/login.php'}  
